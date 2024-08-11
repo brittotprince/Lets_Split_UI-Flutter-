2f132a009711e23bb2511e25ecc2be08b0b7a039
“Britto <bprince@commvault.com>
2024-08-11 20:53:28 +0530

    removing speech to text

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'chat_provider.dart';
-import 'user_message_bubble.dart';
-import 'ai_message_bubble.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -14,47 +12,63 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
   final ImagePicker _picker = ImagePicker();
-  stt.SpeechToText _speech;
-  bool _isListening = false;
-  String _voiceInput = '';
+  FlutterSoundRecorder? _recorder;
+  FlutterSoundPlayer? _player;
+  bool _isRecording = false;
+  String? _audioPath;
 
   @override
   void initState() {
     super.initState();
-    _speech = stt.SpeechToText();
-    _fetchMessages();
+    _recorder = FlutterSoundRecorder();
+    _player = FlutterSoundPlayer();
+    _initializeRecorder();
   }
 
-  Future<void> _fetchMessages() async {
-    await Provider.of<ChatProvider>(context, listen: false).fetchMessages();
+  Future<void> _initializeRecorder() async {
+    await _recorder!.openAudioSession();
+    await _player!.openAudioSession();
+    await Permission.microphone.request();
   }
 
   Future<void> _pickImage() async {
-    final pickedFile = await _picker.getImage(source: ImageSource.gallery);
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      Provider.of<ChatProvider>(context, listen: false)
-          .addImageMessage(pickedFile.path, 'user');
+      File imageFile = File(pickedFile.path);
+      // Send imageFile to backend
     }
   }
 
-  Future<void> _startListening() async {
-    bool available = await _speech.initialize();
-    if (available) {
-      setState(() => _isListening = true);
-      _speech.listen(onResult: (val) => setState(() {
-            _voiceInput = val.recognizedWords;
-          }));
+  Future<void> _startRecording() async {
+    if (!_isRecording) {
+      _audioPath = await _recorder!.startRecorder(toFile: 'audio.aac');
+      setState(() {
+        _isRecording = true;
+      });
     }
   }
 
-  void _stopListening() {
-    _speech.stop();
-    setState(() => _isListening = false);
-    if (_voiceInput.isNotEmpty) {
-      Provider.of<ChatProvider>(context, listen: false)
-          .addVoiceMessage(_voiceInput, 'user');
-      _voiceInput = '';
+  Future<void> _stopRecording() async {
+    if (_isRecording) {
+      await _recorder!.stopRecorder();
+      setState(() {
+        _isRecording = false;
+      });
+      // Send _audioPath to backend
     }
+  }
+
+  Future<void> _playAudio() async {
+    if (_audioPath != null) {
+      await _player!.startPlayer(fromURI: _audioPath);
+    }
+  }
+
+  @override
+  void dispose() {
+    _recorder!.closeAudioSession();
+    _player!.closeAudioSession();
+    super.dispose();
   }
 
   @override
@@ -66,54 +80,41 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           Expanded(
-            child: Consumer<ChatProvider>(
-              builder: (context, chatProvider, child) {
-                return ListView.builder(
-                  itemCount: chatProvider.messages.length,
-                  itemBuilder: (context, index) {
-                    final message = chatProvider.messages[index];
-                    if (message['sender'] == 'user') {
-                      return UserMessageBubble(message: message);
-                    } else {
-                      return AIMessageBubble(message: message);
-                    }
-                  },
-                );
-              },
+            child: ListView(
+              children: [
+                // Your chat messages here
+              ],
             ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.image),
+                onPressed: _pickImage,
+              ),
+              IconButton(
+                icon: Icon(_isRecording ? Icons.stop : Icons.mic),
+                onPressed: _isRecording ? _stopRecording : _startRecording,
+              ),
+              IconButton(
+                icon: Icon(Icons.play_arrow),
+                onPressed: _playAudio,
+              ),
+            ],
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.image),
-                  onPressed: _pickImage,
-                ),
-                IconButton(
-                  icon: Icon(_isListening ? Icons.mic : Icons.mic_none),
-                  onPressed: _isListening ? _stopListening : _startListening,
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: 'Type a message',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                IconButton(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'Enter your message',
+                suffixIcon: IconButton(
                   icon: Icon(Icons.send),
                   onPressed: () {
-                    if (_controller.text.isNotEmpty) {
-                      Provider.of<ChatProvider>(context, listen: false)
-                          .sendMessage(_controller.text, 'user');
-                      _controller.clear();
-                    }
+                    // Send text message to backend
                   },
                 ),
-              ],
+              ),
             ),
           ),
         ],
