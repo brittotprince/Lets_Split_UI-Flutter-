28c9450d6508c6104dd065316e8f022b4c99541d
“Britto <bprince@commvault.com>
2024-08-11 20:12:39 +0530

    Chat functionality to include image and voice input using the image_picker and speech_to_text packages.

--- lib/chat_provider.dart
+++ lib/chat_provider.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ChatProvider with ChangeNotifier {
-  List<Map<String, String>> _messages = [];
+  List<Map<String, dynamic>> _messages = [];
 
-  List<Map<String, String>> get messages => _messages;
+  List<Map<String, dynamic>> get messages => _messages;
 
   void addMessage(String message, String sender) {
-    _messages.add({'message': message, 'sender': sender});
+    _messages.add({'type': 'text', 'message': message, 'sender': sender});
+    notifyListeners();
+  }
+
+  void addImageMessage(String imagePath, String sender) {
+    _messages.add({'type': 'image', 'imagePath': imagePath, 'sender': sender});
+    notifyListeners();
+  }
+
+  void addVoiceMessage(String voicePath, String sender) {
+    _messages.add({'type': 'voice', 'voicePath': voicePath, 'sender': sender});
     notifyListeners();
   }
 }
--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'chat_provider.dart';
 import 'user_message_bubble.dart';
 import 'ai_message_bubble.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _voiceInput = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.getImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      Provider.of<ChatProvider>(context, listen: false)
+          .addImageMessage(pickedFile.path, 'user');
+    }
+  }
+
+  Future<void> _startListening() async {
+    bool available = await _speech.initialize();
+    if (available) {
+      setState(() => _isListening = true);
+      _speech.listen(onResult: (val) => setState(() {
+            _voiceInput = val.recognizedWords;
+          }));
+    }
+  }
+
+  void _stopListening() {
+    _speech.stop();
+    setState(() => _isListening = false);
+    if (_voiceInput.isNotEmpty) {
+      Provider.of<ChatProvider>(context, listen: false)
+          .addVoiceMessage(_voiceInput, 'user');
+      _voiceInput = '';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +67,18 @@ class ChatScreen extends StatelessWidget {
                   itemCount: chatProvider.messages.length,
                   itemBuilder: (context, index) {
                     final message = chatProvider.messages[index];
-                    if (message['sender'] == 'user') {
-                      return UserMessageBubble(message: message['message']!);
-                    } else {
-                      return AIMessageBubble(message: message['message']!);
+                    if (message['type'] == 'text') {
+                      if (message['sender'] == 'user') {
+                        return UserMessageBubble(message: message['message']);
+                      } else {
+                        return AIMessageBubble(message: message['message']);
+                      }
+                    } else if (message['type'] == 'image') {
+                      return ImageMessageBubble(imagePath: message['imagePath']);
+                    } else if (message['type'] == 'voice') {
+                      return VoiceMessageBubble(voicePath: message['voicePath']);
                     }
+                    return SizedBox.shrink();
                   },
                 );
               },
@@ -36,6 +88,14 @@ class ChatScreen extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
+                IconButton(
+                  icon: Icon(Icons.image),
+                  onPressed: _pickImage,
+                ),
+                IconButton(
+                  icon: Icon(_isListening ? Icons.mic : Icons.mic_none),
+                  onPressed: _isListening ? _stopListening : _startListening,
+                ),
                 Expanded(
                   child: TextField(
                     controller: _controller,
@@ -59,6 +119,50 @@ class ChatScreen extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ImageMessageBubble extends StatelessWidget {
+  final String imagePath;
+
+  ImageMessageBubble({required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Image.file(File(imagePath)),
+      ),
+    );
+  }
+}
+
+class VoiceMessageBubble extends StatelessWidget {
+  final String voicePath;
+
+  VoiceMessageBubble({required this.voicePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Text(voicePath), // Placeholder for voice message
       ),
     );
   }
